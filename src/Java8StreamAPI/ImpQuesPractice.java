4846b0aa4db7c2e1ad019be672f1d9adb1a5afaf
1012Maani <manishaverma140698@gmail.com>
2023-08-14 16:09:27 +0530

    practice

---
+++ src/Java8StreamAPI/ImpQuesPractice.java
@@ -0,0 +1,103 @@
+package Java8StreamAPI;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import java.sql.Array;
+import java.sql.SQLOutput;
+import java.util.HashMap;
+import java.util.TreeMap;
+import java.util.function.Predicate;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+public class ImpQuesPractice {
+
+
+
+     class Sample1 {
+        public static void main(String[] args) {
+//        String s1="ABC";
+//        String s2=new String("ABC");
+//        String s3="ABC";
+//        System.out.println(s1==s3);
+//        System.out.println(s1.equals(s2));
+
+//        TreeMap<Integer,String> map=new TreeMap<>();
+//        map.put(1,"A");
+//        map.put(2,"C");
+//        map.put(null,"D");
+//        map.put(null,"P");
+//        System.out.println(map);
+
+            List<Integer> list=new ArrayList();
+            list.add(2);
+            list.add(4);
+            list.add(5);
+            list.add(8);
+            list.add(9);
+            Predicate<Integer> p= item-> item%2==0;
+            list.stream().filter(p).map(item-> item*item).collect(Collectors.toList());
+//        Predicate <Integer> p= item-> item%2>0 ;
+
+
+
+        }
+    }
+
+     class A {
+
+        //    static int x;
+//    int y;
+        static{
+            System.out.println("Hi");
+        }
+        static void print()
+        {
+            System.out.println("Hola");
+        }
+        public static void main(String[] args) {
+            System.out.println("Hello");
+
+        }
+
+        static
+        {
+            System.out.println("HI, Hello");
+        }
+    }
+  class Sample {
+
+        public void add(int x,int y)
+        {
+            int result=x+y;
+            System.out.println(result);
+        }
+        public void add(float x, float y)
+        {
+            float result=x+y;
+            System.out.println(result);
+        }
+    }
+    abstract class Add
+    {
+        public abstract void add();
+        public void sample()
+        {
+            System.out.println("Hello");
+        }
+        public abstract void multiple();
+
+    }
+    abstract class ImplClass extends Add
+    {
+        public void add()
+        {
+            System.out.println("Addition");
+        }
+
+    }
+
+
+}
--- src/Java8StreamAPI/Stream_API.java
+++ src/Java8StreamAPI/Stream_API.java
@@ -2,6 +2,9 @@ package Java8StreamAPI;
 import java.util.*;
 import java.util.stream.Collectors;
 
+import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.maxBy;
+
 public class Stream_API {
 //    Q1. Print the avg age of all the people
 //    Ans1: Double result= people.stream().collect(Collectors.averagingInt(Person::getAge));
@@ -19,5 +22,34 @@ public static boolean containsVowel(String name)
 }
     //-----------------------------------------------------------------------------
     // Q3. Create a list of people, sorted in ascending order on the basis of age, if age is the same then sort in descending order of name
-    // Ans:
+    // Ans: List<Person> sortedPeople = people.stream()
+    //                .sorted(Comparator.comparingInt(Person::getAge).thenComparing(Comparator.comparing(Person::getName).reversed()))
+    //                .collect(Collectors.toList());
+    //        System.out.println(sortedPeople);
+
+    //q4: Create a map from this people list where the key is country name and value is count which means a map will tell how many people live in a particular country
+    //ans: Map<String, Long> map= people.stream()
+    //                .collect(Collectors.groupingBy(Person::getCountry, Collectors.counting()));
+    //        System.out.println(map);
+
+    //q:5 Create a map which stores avg age of people per country (key should be country and value should be average age i.e, double)
+//Map<String, Double> map= people.stream().collect(Collectors.groupingBy(Person::getCountry,Collectors.averagingDouble(Person::getAge)));
+//        System.out.println(map);
+
+    //q6:Print the oldest person in every country
+//    Map<String, Optional<Person>> oldestPersonByCountry = people.stream()
+//            .collect(groupingBy(Person::getCountry, maxBy(Comparator.comparing(Person::getAge))));
+
+
+ //q7:Map.Entry<String, Long> mostPopulatedCountry = people.stream()
+    //                .collect(Collectors.groupingBy(Person::getCountry, Collectors.counting()))
+    //                .entrySet().stream()
+    //                .max(Comparator.comparingLong(Map.Entry::getValue))
+    //                .get();
+    //        System.out.println(mostPopulatedCountry.getKey());
+
+    //q8Create a list of 20 random integers in the range 0 - 1000 using Java 8 streams
+
+
+
 }
